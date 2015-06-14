809e0389666b79c0be6fd5034e7aacde32b21d9e
Jade <dongqiaoruby@gmail.com>
2015-06-14 12:12:53 +0800

    init project

---
+++ src/user/DBServlet.java
@@ -0,0 +1,61 @@
+package user;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DBServlet
+ */
+@WebServlet("/DBServlet")
+public class DBServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	//用于
+	protected java.sql.Connection conn = null;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DBServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    //执行各种sql语句的方法
+    protected java.sql.ResultSet execSQL(String sql, Object... args) throws Exception {
+		//建立PreparedStatement对象
+    	java.sql.PreparedStatement pSmt = conn.prepareStatement(sql);
+    	//为pSmt对象设置sql参数
+    	for (int i = 0; i < args.length; i ++){
+    		pSmt.setObject(i + 1, args[i]);
+    	}
+    	return null;
+    	
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
